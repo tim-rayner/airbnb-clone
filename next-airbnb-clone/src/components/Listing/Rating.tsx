2fe8708640431c4ae53401d38f20fec005410bc3
Tim Rayner <tim.rayner@liftshare.com>
2024-07-07 21:41:05 +0100

    update: updated todo comments for listing rating

--- next-airbnb-clone/src/components/Listing/Rating.tsx
+++ next-airbnb-clone/src/components/Listing/Rating.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+/**
+ * @info this component is being used client side due to the reactivity needed for the below todo.
+ * @todo on click, check if user is logged in, if not, redirect to login page
+ */
 const Rating = ({ rating }: { rating?: string }) => {
   return (
     <div className="items-center flex">
