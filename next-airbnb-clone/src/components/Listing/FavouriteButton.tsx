03a4c563efeb6e8e280a5bcd8368c83a1e0412d4
Tim Rayner <tim.rayner@liftshare.com>
2024-07-09 21:14:53 +0100

    update: added functionality to interact with fav button, needs to be updated to check auth status though

--- next-airbnb-clone/src/components/Listing/FavouriteButton.tsx
+++ next-airbnb-clone/src/components/Listing/FavouriteButton.tsx
@@ -1,13 +1,23 @@
 'use client';
 
+import { useState } from 'react';
+
 /**
  * @info this component is being used client side due to the reactivity needed for the below todo.
  * @todo on click, check if user is logged in, if not, redirect to login page
  */
 const FavouriteButton = ({ rating }: { rating?: string }) => {
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  const textColour = isFavourite ? 'text-red-500' : 'text-black';
+
+  const toggleFavourite = () => {
+    setIsFavourite((prev) => !prev);
+  };
+
   return (
-    <div className="fav-button opacity-75 z-50">
-      <i className="pi pi-heart-fill text-black text-2xl mr-2"></i>
+    <div className="fav-button opacity-75 z-50" onClick={toggleFavourite}>
+      <i className={`pi pi-heart-fill text-2xl mr-2 ${textColour}`}></i>
     </div>
   );
 };
