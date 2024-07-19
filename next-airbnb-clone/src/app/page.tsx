2cf7c87c53c822900c3aae7f8f28eb518d14feef
tim-rayner <tim.rayner2020@gmail.com>
2024-07-19 18:08:35 +0100

    update: ported sandbox layout to its own sandbox page

--- next-airbnb-clone/src/app/page.tsx
+++ next-airbnb-clone/src/app/page.tsx
@@ -1,27 +1,13 @@
-'use client'
-import Button from "@/components/Button"
-import Calendar from "@/components/Calendar/Calendar";
-import CategoryItem from "@/components/CategoryItem";
-import Listing from "@/components/Listing/Listing";
+'use client';
 
-
-const onDateChange = (range: { startDate: Date; endDate: Date; key: string }) => {
+const onDateChange = (range: {
+  startDate: Date;
+  endDate: Date;
+  key: string;
+}) => {
   console.log(range);
-} 
+};
 
 export default function Home() {
-  
-  return (
-    <main className="flex flex-col w-fit ">
-        Component Sandbox:
-        <Button label="Register"  />
-        <div className="flex flex-row">
-          <CategoryItem category={{name: "Houses", icon: "pi pi-home"}} />
-          <CategoryItem category={{name: "Apartments", icon: "pi pi-building"}} />
-          <CategoryItem category={{name: "Condos", icon: "pi pi-star-fill"}} />
-        </div>
-        <Calendar onDateChange={onDateChange} />
-        <Listing />
-    </main>
-  );
+  return <main className="flex flex-col w-fit "></main>;
 }
---
+++ next-airbnb-clone/src/app/sandbox/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+import Button from '@/components/Button';
+import Calendar from '@/components/Calendar/Calendar';
+import CategoryItem from '@/components/CategoryItem';
+import Listing from '@/components/Listing/Listing';
+
+const onDateChange = (range: {
+  startDate: Date;
+  endDate: Date;
+  key: string;
+}) => {
+  console.log(range);
+};
+
+export default function Sandbox() {
+  return (
+    <main className="flex flex-col w-fit ">
+      Component Sandbox:
+      <Button label="Register" />
+      <div className="flex flex-row">
+        <CategoryItem category={{ name: 'Houses', icon: 'pi pi-home' }} />
+        <CategoryItem
+          category={{ name: 'Apartments', icon: 'pi pi-building' }}
+        />
+        <CategoryItem category={{ name: 'Condos', icon: 'pi pi-star-fill' }} />
+      </div>
+      <Calendar onDateChange={onDateChange} />
+      <Listing />
+    </main>
+  );
+}
